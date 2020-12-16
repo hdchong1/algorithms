14cd147a19ae5d222678bee21c3ff8c664f0f858
home <home@192.168.0.12>
2020-12-16 14:49:52 -0500

    Fibonacci sequence

--- src/basic/_2/Main.java
+++ src/basic/_2/Main.java
@@ -8,15 +8,32 @@ public class Main {
 		
 		arr[0] = 1;
 		arr[1] = 1;
-//		arr[2] = 2;
 		
 		for (int i=2; i<100; i++) {
 			arr[i] = arr[i-1] + arr[i-2];
 		}
 		
+		System.out.println("First Method");
 		for (int i=0; i<=10; i++) {
-			System.out.println(arr[i]+" ");
+			System.out.print(arr[i]+" ");
 		}
+		
+// second method		
+		System.out.println();
+		System.out.println("Second Method");
+		
+		int firstPrev = 1;
+		int secondPrev = 1;
+		System.out.print(firstPrev+" ");
+		System.out.print(secondPrev+" ");		
+		
+		for (int i=0; i<9; i++) {
+			int nextNum = firstPrev + secondPrev;
+			firstPrev = secondPrev;
+			secondPrev = nextNum;
+			
+			System.out.print(nextNum+" ");
+		}	
 
 	}
 
