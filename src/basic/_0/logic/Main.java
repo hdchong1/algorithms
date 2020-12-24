b8177c6f42d594e96e7e3421e18c3111cb463b51
home <home@192.168.0.12>
2020-12-23 19:49:39 -0500

    Problem 9. Sum of every digit: add another algorithm using array

--- src/basic/_0/logic/Main.java
+++ src/basic/_0/logic/Main.java
@@ -11,10 +11,16 @@ public class Main {
 		while (isContinue) {
 			
 			// write here
-			CountNumber_14 cntNum = new CountNumber_14();		
-			cntNum.printCountNumber(com.getInputNum(1));
+//			CountNumber_14 cntNum = new CountNumber_14();		
+//			cntNum.printCountNumber(com.getInputNum(1));
 			// end 	
 			
+			// write here
+			SumNumber_9 sumNum = new SumNumber_9();		
+			sumNum.printSumNumber(com.getInputNum(1));
+			sumNum.printSumNumberByCharArray(com.getInputNum(1));
+			// end 	
+
 			isContinue = com.getInputYN();
 		}
 		com.closeScanner();		
--- src/basic/_0/logic/SumNumber_9.java
+++ src/basic/_0/logic/SumNumber_9.java
@@ -1,5 +1,31 @@
 package basic._0.logic;
 
 public class SumNumber_9 {
+	
+	public void printSumNumber(int inputNum){
+		
+		int sum=0;
+		while (inputNum > 0) {
+			
+			sum += (inputNum%10);
+			inputNum /= 10;
+			
+		}
+		
+		System.out.println("Sum is "+sum);
+	}
+	
+	public void printSumNumberByCharArray(int inputNum) {
+		
+		int sum=0;
+		String inputStr = Integer.toString(inputNum);
+		char[] inputChrArr = inputStr.toCharArray();
+		
+		for (char c : inputChrArr) {
+			sum += Character.getNumericValue(c);
+		}
+		
+		System.out.println("Sum is "+sum);
+	}
 
 }
